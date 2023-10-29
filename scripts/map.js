1e8b5143cba9ba70f78c7fd07ff54b61f11872ba
yiyunsh <yiyun.lin@stud.fhgr.ch>
2023-10-29 13:45:55 +0100

    visualize map

---
+++ scripts/map.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var options = {
+        center: { lat: 47.378341957776115, lng: 8.540240971123316 },
+        zoom: 8,
+    }
+    map = new google.maps.Map(document.getElementById("map"), options);
+}
+
+
