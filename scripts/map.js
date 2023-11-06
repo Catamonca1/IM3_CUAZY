73ca9e93b2e2694b71e747625a7c537011ba557e
ChrisFHGR <145985514+ChrisFHGR@users.noreply.github.com>
2023-11-06 08:50:53 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -3,7 +3,36 @@ function initMap() {
         center: { lat: 47.378341957776115, lng: 8.540240971123316 },
         zoom: 8,
     }
-    map = new google.maps.Map(document.getElementById("map"), options);
+    map = new google.maps.Map(document.getElementById("map"), options);    
 }
 
 
+function createMarkers(data) {
+
+    //init the map if its not already inited
+    if (typeof map === 'undefined') {
+        initMap();
+        console.log('inited map')
+    }
+
+    // Loop through your filteredData array and create markers for each school
+    data.forEach(school => {
+        const marker = new google.maps.Marker({
+            position: {
+                lat: parseFloat(school.lat),
+                lng: parseFloat(school.lng),
+            },
+            map: map,
+            // add more options if you want
+        });
+
+        // if you want, you can add an event listener to the marker, 
+        // f.e.: to open an info window when clicked :)
+        marker.addListener('click', function () {
+            const infoWindow = new google.maps.InfoWindow({
+                content: `<div class="infowindow"><strong>${school.name}</strong> <div>${school.website}</div> <div>${school.telephone}</div></div>`
+            });
+            infoWindow.open(map, marker);
+        });
+    });
+}
