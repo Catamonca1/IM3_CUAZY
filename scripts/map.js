117fbedd15e017cfa328097c974eebc980e3ac5c
Catamonca1 <146833840+Catamonca1@users.noreply.github.com>
2023-11-12 16:45:09 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,7 @@
 function initMap() {
     var options = {
         center: { lat: 47.378341957776115, lng: 8.540240971123316 },
-        zoom: 8,
+        zoom: 12,
     }
     map = new google.maps.Map(document.getElementById("map"), options);    
 }
